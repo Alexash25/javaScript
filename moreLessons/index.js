d33ef5808f5e4689f83c4c24bfd6d2c0fdd8e6e8
Alexander Tiu <alextiu0525@gmail.com>
2026-06-26 14:58:00 -0500

    I learned about stuff

--- moreLessons/index.js
+++ moreLessons/index.js
@@ -43,21 +43,45 @@ try {
 
 console.log("You have reached the end!");
 
-try {
-    const dividend = Number(window.prompt("Enter a dividend: "));
-    const divisor = Number(window.prompt("Enter a divisor: "));
+// try {
+//     const dividend = Number(window.prompt("Enter a dividend: "));
+//     const divisor = Number(window.prompt("Enter a divisor: "));
 
-    if (divisor == 0) {
-        throw new Error("You can't divide by zero!");
-    }
+//     if (divisor == 0) {
+//         throw new Error("You can't divide by zero!");
+//     }
 
-    if (isNaN(dividend) || isNaN(divisor)) {
-        throw new Error("Values must be a number!");
-    }
-    const result = dividend / divisor;
-    console.log(result);
-} catch (error) {
-    console.error(error);
-}
+//     if (isNaN(dividend) || isNaN(divisor)) {
+//         throw new Error("Values must be a number!");
+//     }
+//     const result = dividend / divisor;
+//     console.log(result);
+// } catch (error) {
+//     console.error(error);
+// }
 
 console.log("You have reached the end!");
+
+// DOM = Document Object Model
+//       Object{} that represents the page you see in the web browser
+
+// element selectors = Methods used to target and manipulate HTML elements
+
+// 1. document.getElementById()
+// 2. document.getElementsClassName()
+// 3. document.getElementsByTagName()
+// 4. document.querySelector()
+// 5. document.querySelectorAll()
+
+
+// Step 1 Create the Element
+const newH1 = document.createElement("h1");
+
+// Step 2 add attributes and properites
+newH1.textContent = 'I like pizza';
+newH1.id ="myH1";
+
+// Step 3 Append to the DOM
+document.body.append(newH1);
+
+// eventListener = LIsten for specific events to create interactive web pages
